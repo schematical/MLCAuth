bc9d4569cfaf3af65651cddbe5cc7849f93a7da7
Matt Lea <mlea@schematical.com>
2013-07-20 13:34:28 -0500

    xxx

--- _core/ctl/MLCLoginPanel.class.php
+++ _core/ctl/MLCLoginPanel.class.php
@@ -22,24 +22,27 @@ class MLCLoginPanel extends MJaxPanel{
 		
         $this->txtEmail = new MJaxTextBox($this);
 		$this->txtEmail->Attr('placeholder','Email');
-		$this->txtEmail->AddCssClass('span3');
+		//$this->txtEmail->AddCssClass('span3');
 		
         $this->txtPass = new MJaxTextBox($this);
         $this->txtPass->TextMode = MJaxTextMode::Password;
 		$this->txtPass->Attr('placeholder','Password');
         $this->txtPass->AddAction(new MJaxEnterKeyEvent(), new MJaxServerControlAction($this, 'btnSubmit_click'));
-		$this->txtPass->AddCssClass('span3');
+		//$this->txtPass->AddCssClass('span3');
         
         $this->btnSubmit = new MJaxButton($this);
         $this->btnSubmit->AddAction(new MJaxClickEvent(), new MJaxServerControlAction($this, 'btnSubmit_click'));
 		$this->btnSubmit->Text = "Login";
-		$this->btnSubmit->AddCssClass('btn');
+		//$this->btnSubmit->AddCssClass('btn');
 
     }
     public function btnSubmit_click($strFormId, $strControlId, $strActionParameter){
         $strEmail = $this->txtEmail->Text;
 		if(!filter_var($strEmail, FILTER_VALIDATE_EMAIL)){
-			$this->divAlert->Text = "Email is not valid";
+			$this->objForm->CtlAlert(
+                $this->txtEmail->ControlId . '_holder',
+                "Email is not valid"
+            );
 			return;
 		}
 		
@@ -50,22 +53,21 @@ class MLCLoginPanel extends MJaxPanel{
         $blnSuccess = MLCAuthDriver::Authenticate($strEmail, $strPassword);
         if($blnSuccess){
            $this->TriggerEvent('auth_login');
-           $this->divAlert->Text = "Success";
+            $this->objForm->CtlAlert(
+                $this->txtPass->ControlId . '_holder',
+                "Email and Password Do Not Match",
+                'success'
+            );
+            return false;
 			
 		   $this->objForm->Redirect($this->strRedirectUrl);
         }else{
-        	try{
-        		$objUser = MLCAuthDriver::CreateUser($strEmail, $strPassword);
-				$blnSuccess = MLCAuthDriver::Authenticate($strEmail, $strPassword);
-				if($blnSuccess){
-					$this->objForm->Redirect($this->strRedirectUrl);
-				}else{
-					$this->divAlert->Text = "Could not create user";
-				}
-        	}catch(MLCAuthException $e){
-        		$this->divAlert->Text = "Email and Password Do Not Match";
-				return false;
-        	}
+            $this->objForm->CtlAlert(
+                $this->txtPass->ControlId . '_holder',
+                "Email and Password Do Not Match"
+            );
+            return false;
+
             
         }
 	
